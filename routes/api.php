e550d87f383c370e0a7d2e1282733963199d209d
nicortezm <nascorman@gmail.com>
2024-12-25 00:51:12 -0300

    feat: :sparkles: update environment configuration for MySQL and add Docker setup with entrypoint script

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,34 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AppointmentController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::get('/', function () {
+    return 'OK';
+});
+
+Route::group(['prefix' => 'appointments'], function () {
+    Route::post('/confirm-appointment', [AppointmentController::class, 'confirmAppointment'])->middleware('auth:sanctum');
+    Route::get('/view-appointments', [AppointmentController::class, 'viewTodayAppointments'])->middleware('auth:sanctum');
+});
+
+Route::apiResource('appointments', AppointmentController::class)->middleware('auth:sanctum');
+Route::post('/create-doctor', [AuthController::class, 'createDoctor'])->middleware('auth:sanctum');
+
+Route::group(['prefix' => 'payments'], function () {
+    Route::get('/link/{id}', [PaymentController::class, 'getPaymentlink'])->middleware('auth:sanctum');
+    Route::get('/callback-success', [PaymentController::class, 'callbackMPSuccess']);
+    Route::get('/callback-failure', [PaymentController::class, 'callbackMPFailure']);
+});
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+});
+;
